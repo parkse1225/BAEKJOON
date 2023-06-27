19f9779d3114e5c3e70cb4eb7edb1b04f2782218
parkse1225 <jiny6209@naver.com>
2023-06-27 20:21:19 +0900

    BAEKJOON3

---
+++ src/B3_1001.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+ 
+public class B3_1001 {
+ 
+	public static void main(String[] args) {
+ 
+        Scanner in = new Scanner(System.in);
+		int A = in.nextInt();
+		int B = in.nextInt();
+		
+		System.out.println(A-B);
+ 
+		in.close();
+	}
+}
+
+// ---------------------방법 2-1 --------------------- //
+ 
+/* import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+        
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+ 		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str," ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a-b);
+	}
+} */
+
+/*    
+굳이 String 변수 생성 안하고 입력과 동시에 구분자로 분리해줘도 된다.
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a-b); 
+        
+*/
